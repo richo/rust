4ef50d703e1dc3c66eb03778c8a90030fcfd073d
Richo Healey <richo@psych0tik.net>
2014-10-08 11:41:54 -0700

    Use the prerelease version

--- stash.sh
+++ stash.sh
@@ -2,6 +2,11 @@
 set -xe
 
 rust_version=$(head -n 1 RELEASES.txt | cut -f 2 -d " ")
+major=$(echo "$rust_version" | cut -f 1 -d '.')
+minor=$(echo "$rust_version" | cut -f 2 -d '.')
+patch=$(echo "$rust_version" | cut -f 3 -d '.')
+
+rust_version="${major}.$(expr $minor + 1).${patch}"
 
 rev=`git rev-parse --short @`
 branch=$(git symbolic-ref HEAD | sed -e 's|refs/heads/||')
