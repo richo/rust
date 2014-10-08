e24578a5377f1edede20d1e20c227eb3efa5692e
Richo Healey <richo@psych0tik.net>
2014-10-08 11:38:40 -0700

    Be smarter with naming rusts

--- stash.sh
+++ stash.sh
@@ -4,11 +4,17 @@ set -xe
 rust_version=$(head -n 1 RELEASES.txt | cut -f 2 -d " ")
 
 rev=`git rev-parse --short @`
-prefix="~/.rusts/$rust_version-$rev"
+branch=$(git symbolic-ref HEAD | sed -e 's|refs/heads/||')
+
+if [ "$branch" != "master" ]; then
+    prefix="~/.rusts/$rust_version-$branch"
+else
+    prefix="~/.rusts/$rust_version-$rev"
+fi
 
 ./configure --prefix=$prefix
 make -j4
 make install
 
 set +x
-echo "You may want to update your dotfiles to use '$rust_version-$rev'"
+echo "You may want to update your dotfiles to use '$(basename ${prefix})'"
