3c9aa7d2f27b22424fe435d4c55cbf9588957142
Richo Healey <richo@psych0tik.net>
2014-10-30 18:39:50 -0700

    setup the power target

--- src/librustc_llvm/lib.rs
+++ src/librustc_llvm/lib.rs
@@ -1897,6 +1897,11 @@ extern {
     pub fn LLVMInitializeMipsTargetMC();
     pub fn LLVMInitializeMipsAsmPrinter();
     pub fn LLVMInitializeMipsAsmParser();
+    pub fn LLVMInitializePowerPCTargetInfo();
+    pub fn LLVMInitializePowerPCTarget();
+    pub fn LLVMInitializePowerPCTargetMC();
+    pub fn LLVMInitializePowerPCAsmPrinter();
+    pub fn LLVMInitializePowerPCAsmParser();
 
     pub fn LLVMRustAddPass(PM: PassManagerRef, Pass: *const c_char) -> bool;
     pub fn LLVMRustCreateTargetMachine(Triple: *const c_char,
@@ -2165,6 +2170,12 @@ pub unsafe fn static_link_hack_this_sucks() {
     LLVMInitializeMipsTargetMC();
     LLVMInitializeMipsAsmPrinter();
     LLVMInitializeMipsAsmParser();
+
+    LLVMInitializePowerPCTargetInfo();
+    LLVMInitializePowerPCTarget();
+    LLVMInitializePowerPCTargetMC();
+    LLVMInitializePowerPCAsmPrinter();
+    LLVMInitializePowerPCAsmParser();
 
     LLVMRustSetLLVMOptions(0 as c_int,
                                        0 as *const _);
