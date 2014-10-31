69468c25372c6c2118071939e86e31ef540b7bd2
Richo Healey <richo@psych0tik.net>
2014-10-30 18:52:29 -0700

    Revert "WIP"
    This reverts commit 8da889f6fcc225abd51c6a2d42664b67c77f430b.

--- src/librustc_trans/back/link.rs
+++ src/librustc_trans/back/link.rs
@@ -1100,7 +1100,6 @@ fn add_local_native_libraries(cmd: &mut Command, sess: &Session) {
     }
 
     for &(ref l, kind) in others {
-        println!("using lib: {}", l);
         match kind {
             cstore::NativeUnknown => {
                 cmd.arg(format!("-l{}", l));
