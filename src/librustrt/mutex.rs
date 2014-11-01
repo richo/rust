458fbf6e0cea08d714b9db8b4e983bbafad4ccb7
Richo Healey <richo@psych0tik.net>
2014-11-01 05:46:50 -0700

    FIXME: More powerpc fixes

--- src/librustrt/mutex.rs
+++ src/librustrt/mutex.rs
@@ -415,6 +415,8 @@ mod imp {
         const __SIZEOF_PTHREAD_MUTEX_T: uint = 24 - 8;
         #[cfg(target_arch = "mipsel")]
         const __SIZEOF_PTHREAD_MUTEX_T: uint = 24 - 8;
+        #[cfg(target_arch = "powerpc")]
+        const __SIZEOF_PTHREAD_MUTEX_T: uint = 24 - 8;
         #[cfg(target_arch = "x86_64")]
         const __SIZEOF_PTHREAD_COND_T: uint = 48 - 8;
         #[cfg(target_arch = "x86")]
@@ -426,7 +428,7 @@ mod imp {
         #[cfg(target_arch = "mipsel")]
         const __SIZEOF_PTHREAD_COND_T: uint = 48 - 8;
         #[cfg(target_arch = "powerpc")]
-        const __SIZEOF_PTHREAD_COND_T: uint = 24 - 8;
+        const __SIZEOF_PTHREAD_COND_T: uint = 48 - 8;
 
         #[repr(C)]
         pub struct pthread_mutex_t {
