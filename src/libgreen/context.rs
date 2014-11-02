91cbb7dca1cec51ca50a97d372ebcdd51b633b23
Richo Healey <richo@psych0tik.net>
2014-11-02 00:00:49 -0700

    FIXME: Add janky and broken libgreen shim for power

--- src/libgreen/context.rs
+++ src/libgreen/context.rs
@@ -186,6 +186,41 @@ fn initialize_call_frame(regs: &mut Registers, fptr: InitFn, arg: uint,
     regs.ebp = 0;
 }
 
+#[cfg(target_arch = "powerpc")]
+#[repr(C)]
+struct Registers {
+    r0: u32, r1: u32, r2: u32, r3: u32,
+    r4: u32, r5: u32, r6: u32, r7: u32,
+    r8: u32, r9: u32, r10: u32, r11: u32,
+    r12: u32, r13: u32, r14: u32, r15: u32,
+    r16: u32, r17: u32, r18: u32, r19: u32,
+    r20: u32, r21: u32, r22: u32, r23: u32,
+    r24: u32, r25: u32, r26: u32, r27: u32,
+    r28: u32, r29: u32, r30: u32, r31: u32,
+    pc: u32, cr: u32
+}
+
+#[cfg(target_arch = "powerpc")]
+fn new_regs() -> Box<Registers> {
+    box Registers {
+        r0: 0, r1: 0, r2: 0, r3: 0,
+        r4: 0, r5: 0, r6: 0, r7: 0,
+        r8: 0, r9: 0, r10: 0, r11: 0,
+        r12: 0, r13: 0, r14: 0, r15: 0,
+        r16: 0, r17: 0, r18: 0, r19: 0,
+        r20: 0, r21: 0, r22: 0, r23: 0,
+        r24: 0, r25: 0, r26: 0, r27: 0,
+        r28: 0, r29: 0, r30: 0, r31: 0,
+        pc: 0, cr: 0
+    }
+}
+
+#[cfg(target_arch = "powerpc")]
+fn initialize_call_frame(regs: &mut Registers, fptr: InitFn, arg: uint,
+                         procedure: raw::Procedure, sp: *mut uint) {
+    // YOLO
+}
+
 // windows requires saving more registers (both general and XMM), so the windows
 // register context must be larger.
 #[cfg(all(windows, target_arch = "x86_64"))]
